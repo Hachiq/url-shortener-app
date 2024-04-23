4da63227139f2790fa9f7acec3140a3b74233ec9
Hachiq <lesiv433@gmail.com>
2024-04-24 00:57:07 +0300

    Update header based on authentication status

--- Client/src/app/layout/header/header.component.ts
+++ Client/src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  constructor(public authService: AuthService, public tokenService: TokenService) {}
 
 }
