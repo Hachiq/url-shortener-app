ff18577bacb4154e0b6fa5b63e8556632f320283
Hachiq <lesiv433@gmail.com>
2024-04-24 21:02:17 +0300

    Add AuthGuard for details page

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RegisterPageComponent } from './pages/register/register-page/register-p
 import { ShortUrlsTablePageComponent } from './pages/shortUrlsTable/short-urls-table-page/short-urls-table-page.component';
 import { AboutPageComponent } from './pages/about/about-page/about-page.component';
 import { ShortUrlInfoPageComponent } from './pages/shortUrlInfo/short-url-info-page/short-url-info-page.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '',   redirectTo: 'table', pathMatch: 'full' },
@@ -12,7 +13,7 @@ const routes: Routes = [
   { path: "register", component: RegisterPageComponent },
   { path: "login", component: LoginPageComponent },
   { path: "about", component: AboutPageComponent },
-  { path: "details/:id", component: ShortUrlInfoPageComponent }
+  { path: "details/:id", component: ShortUrlInfoPageComponent, canActivate:[AuthGuard] }
 ];
 
 @NgModule({
